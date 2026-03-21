f83b4c4eb77ce214ed23cdec2d2ac1f60775da68
Jinxi Chen <zeguyouyi0210@gmail.com>
2026-03-21 15:32:47 +1100

    Add a new scene for testing

--- CarSalesAssemblyDungeonCrawlerHorror/Assets/Scripts/BreathingMeter.cs
+++ CarSalesAssemblyDungeonCrawlerHorror/Assets/Scripts/BreathingMeter.cs
@@ -1,15 +1,34 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
 
 public class BreathingMeter : MonoBehaviour
 {
     public float breathMeterValue;
     public float breathMeterMaxValue = 100f;
+//U can modify the rate in the projectManager...thing with this script.
     [SerializeField] private float breathMeterDecreaseRate = 5f; 
+
+    [SerializeField] private float pumpIncreaseAmount = 5f; // Amount to increase the breath meter when pumping
+
+//Rendering part!!!!!!!!
+    public TMP_Text BreathingPercentageText;
+    public Slider BreathingSlider;
+
+
 
     bool isDead = false;
     void Start()
     {
         breathMeterValue = breathMeterMaxValue;
+    }
+    public void pump()
+    {
+        if (!isDead&& breathMeterValue < breathMeterMaxValue)
+        {
+            breathMeterValue = Mathf.Min(breathMeterValue + pumpIncreaseAmount, breathMeterMaxValue);
+        }
     }
 
     // Update is called once per frame
@@ -22,16 +41,18 @@ public class BreathingMeter : MonoBehaviour
         else
         {
             breathMeterValue = 0;
-            bool isDead = true;
+            isDead = true;
         }
-        
-    }
-    public void pump()
-    {
-        if (!isDead&& breathMeterValue < breathMeterMaxValue)
+    //pump..
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            breathMeterValue = Mathf.Min(breathMeterValue + 20f, breathMeterMaxValue);
+            pump();
         }
+
+        // Update rendering!
+        BreathingSlider.value = breathMeterValue / breathMeterMaxValue;
+        BreathingPercentageText.text = Mathf.RoundToInt((breathMeterValue / breathMeterMaxValue) * 100) + "%";
+        
     }
 
 }
